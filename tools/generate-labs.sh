7d6b3f8128f01e9befe27d484fb00ae3fedf5fa2
Pranav Salunke <dguitarbite@gmail.com>
2016-02-18 10:21:56 +0100

    Tools: Modifies tar/zip file generation
    Modifying tar/zip generation script for functioning as expected from
    the jenkins publish infra job.
    
    Also fixes a minor potential issue with non-escaped '*' chars.
    
    Change-Id: I375ebcc0e404a7c4786906d4cc693551856df0aa

--- tools/generate-labs.sh
+++ tools/generate-labs.sh
@@ -5,20 +5,31 @@
 #       User by tox.ini to create required scripts and tar balls for
 #       hosting the contents of this project.
 #
+# Note: Execute this script from the root of the repository for expected
+#       results.
 
 set -o nounset  # Treat unset variables as an error
+
+# This variable is fed by openstack-ci post job as a trigger.
+# The value here will be stable/<release-name>
+release=$1
+# Replace '/' with '-' for renaming stable/<release-name> to
+# stalbe-<release-name>
+release=${release////-}
+
+# Create required folders
+mkdir -p ./build/dist/
 
 # Create tarball of labs scripts for Linux/OS X users
 tar --exclude=*.iso \
     --exclude=*.vdi \
     --exclude=*.log \
     --exclude=*.auto \
-    -czf build/labs.tgz labs/osbash
+    -czf build/dist/labs-${release}.tgz labs/osbash
 
 
 # Generate Windows batch scripts
 labs/osbash/osbash.sh -w cluster
 # Create zip file of labs scripts for Windows users
-zip -r build/labs.zip labs/osbash \
-    -x *.iso *.vdi *.log *.auto
-
+zip -r build/dist/labs-${release}.zip labs/osbash \
+    -x *.iso \*.vdi \*.log \*.auto
