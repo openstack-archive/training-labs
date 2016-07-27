openstack-archive/training-labs

commit 3678ef2fa9893468604411ff697361bbd48d1e73

Prevent snapshot name collisions
Skip snapshot taking if a snapshot with the given name already exists.

This is mostly useful when rebuilding (the remainder of) a cluster from
a snapshot. We do not want another series of snapshots for every test
run.

To that end, call the new vm_conditional_snapshot from generic code and
use new functions (vm_snapshot_list, vm_snapshot_exists) in back end
specific libraries to do the work.

Change-Id: If1de62bdf4272e3c9e1a198a104b1c2096ac01f7

labs/osbash/lib/osbash/functions-host.sh
@@ -44,6 +44,18 @@ function get_distro_name {
     fi
 }
 
+# Wrapper around vm_snapshot to deal with collisions with cluster rebuilds
+# starting from snapshot. We could delete the existing snapshot first,
+# rename the new one, or just skip the snapshot.
+function vm_conditional_snapshot {
+    local vm_name=$1
+    local shot_name=$2
+
+    if ! vm_snapshot_exists "$vm_name" "$shot_name"; then
+        vm_snapshot "$vm_name" "$shot_name"
+    fi
+}
+
 #-------------------------------------------------------------------------------
 # Networking
 #-------------------------------------------------------------------------------
@@ -377,8 +389,8 @@ function command_from_config {
             # Format: snapshot [-n <node_name>] <snapshot_name>
             get_cmd_options $args
             local shot_name=$args
-            echo >&2 vm_snapshot "$vm_name" "$shot_name"
-            vm_snapshot "$vm_name" "$shot_name"
+            echo >&2 vm_conditional_snapshot "$vm_name" "$shot_name"
+            vm_conditional_snapshot "$vm_name" "$shot_name"
             ;;
         wait_for_shutdown)
             # Format: wait_for_shutdown [-n <node_name>]
@@ -399,7 +411,7 @@ function command_from_config {
             _autostart_queue "shutdown.sh"
             _vm_boot_with_autostart "$vm_name"
             vm_wait_for_shutdown "$vm_name"
-            vm_snapshot "$vm_name" "$shot_name"
+            vm_conditional_snapshot "$vm_name" "$shot_name"
             ;;
         init_node)
             # Format: init_node [-n <node_name>]

labs/osbash/lib/osbash/kvm-functions.sh
@@ -108,6 +108,10 @@ function stop_running_cluster_vms {
     done
 }
 
+function vm_snapshot_exists {
+    : # Not implemented
+}
+
 function vm_snapshot {
     : # Not implemented
 }

labs/osbash/lib/osbash/virtualbox-functions.sh
@@ -119,6 +119,20 @@ function vm_snapshot_list_tree {
     $VBM snapshot "$vm_name" list 2>/dev/null || rc=$?
 }
 
+function vm_snapshot_list {
+    local vm_name=$1
+
+    # Hide VBM error on stderr if no snapshots exist
+    $VBM snapshot "$vm_name" list --machinereadable 2>/dev/null
+}
+
+function vm_snapshot_exists {
+    local vm_name=$1
+    local shot_name=$2
+
+    vm_snapshot_list "$vm_name" | grep -q "SnapshotName.*=\"$shot_name\""
+}
+
 function vm_snapshot {
     local vm_name=$1
     local shot_name=$2

commit 2dedea435e6ed1de22cf9588ad0775ec5e503277

Do not try to check for existing snapshots on Windows
There is no code to check for existing snapshots on Windows. Listing
them for that purpose just produces an error condition if the snapshot
does not exist (the common case) and the build aborts.

This patch removes the snapshot listing call from the Windows batch
files; it fixes a regression introduced with commit
3678ef2fa9893468604411ff697361bbd48d1e73.

Closes-Bug: #1606766
Change-Id: I221cabdf5e9948dda81cefdff7c8f3b3ab96817c

labs/osbash/lib/osbash/functions-host.sh
@@ -51,7 +51,8 @@ function vm_conditional_snapshot {
     local vm_name=$1
     local shot_name=$2
 
-    if ! vm_snapshot_exists "$vm_name" "$shot_name"; then
+    # On Windows, don't test; take snapshots unconditionally
+    if ! WBATCH= vm_snapshot_exists "$vm_name" "$shot_name"; then
         vm_snapshot "$vm_name" "$shot_name"
     fi
 }