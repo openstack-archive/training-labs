6b5b3feb9612ea23522753d9fb4b099241311a34
Roger Luethi <rl@patchworkscience.org>
2017-03-02 17:41:55 +0100

    Fix copied openrc files
    Our openrc files load some values from configuration files. For the
    copies in the home directory, the relative paths are wrong.
    
    This patch replaces the variables with constants which a) ensures
    that the file works regardless of its path and b) is easier to
    understand for the user.
    
    We also replace the "controller" hostname in the openrc files with
    the equivalent IP address so the openrc files works also on the
    user's host machine.
    
    Change-Id: I237f2449f8d2d5bd52166b8267b384900935b133

--- labs/osbash/config/demo-openstackrc.sh
+++ labs/osbash/config/demo-openstackrc.sh
@@ -1,9 +1,6 @@
 # The variables in this file are exported for use by OpenStack client
 # applications.
 
-# Unlike a regular openstackrc.sh file, this file gets its variable values
-# from other configuration files (to limit redundancy).
-
 # Use BASH_SOURCE so the file works when sourced from a shell, too
 CONFIG_DIR=$(dirname "$BASH_SOURCE")
 source "$CONFIG_DIR/openstack"
--- labs/osbash/scripts/osbash/copy_openrc.sh
+++ labs/osbash/scripts/osbash/copy_openrc.sh
@@ -7,10 +7,34 @@ set -o errexit -o nounset
 TOP_DIR=$(cd "$(dirname "$0")/.." && pwd)
 source "$TOP_DIR/config/paths"
 source "$LIB_DIR/functions.guest.sh"
+source "$CONFIG_DIR/openstack"
+source "$CONFIG_DIR/credentials"
 
 indicate_current_auto
 
 exec_logfile
 
-cp -av "$CONFIG_DIR/admin-openstackrc.sh" "$HOME/admin-openrc.sh"
-cp -av "$CONFIG_DIR/demo-openstackrc.sh" "$HOME/demo-openrc.sh"
+# We replace the hostname "controller" with the equivalent IP address to
+# make the openrc file work on the user's host machine without them
+# changing their /etc/hosts
+MY_MGMT_IP=$(get_node_ip_in_network "$(hostname)" "mgmt")
+
+# Replace variables with constants and remove lines used to fill in variables
+
+cat "$CONFIG_DIR/admin-openstackrc.sh" | sed -e "
+    /BASH_SOURCE/d
+    /^source/d
+    s/\$ADMIN_PROJECT_NAME/$ADMIN_PROJECT_NAME/
+    s/\$ADMIN_USER_NAME/$ADMIN_USER_NAME/
+    s/\$ADMIN_PASS/$ADMIN_PASS/
+    s/controller/$MY_MGMT_IP/
+    " > "$HOME/admin-openrc.sh"
+
+cat "$CONFIG_DIR/demo-openstackrc.sh" | sed -e "
+    /BASH_SOURCE/d
+    /^source/d
+    s/\$DEMO_PROJECT_NAME/$DEMO_PROJECT_NAME/
+    s/\$DEMO_USER_NAME/$DEMO_USER_NAME/
+    s/\$DEMO_PASS/$DEMO_PASS/
+    s/controller/$MY_MGMT_IP/
+    " > "$HOME/demo-openrc.sh"
