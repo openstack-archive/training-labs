e4d0fe2a9806f701bd8755c16ac7fedee3059b70
Roger Luethi <rl@patchworkscience.org>
2017-05-06 15:14:44 +0200

    Clean up openrc copies
    When writing the openrc copies that we put into the osbash user's home
    directory for the users to find, we pick up superfluous comment lines
    from the source file that lack the appropriate context in the resulting
    openrc file.
    
    This patch changes the logic so only the lines we need (starting with
    "export") are kept. This also gets us closer to the install-guide.
    
    Change-Id: If60479adbfecf9713b9981529fb369b13a5c89ad

--- labs/osbash/scripts/osbash/copy_openrc.sh
+++ labs/osbash/scripts/osbash/copy_openrc.sh
@@ -17,24 +17,22 @@ exec_logfile
 # We replace the hostname "controller" with the equivalent IP address to
 # make the openrc file work on the user's host machine without them
 # changing their /etc/hosts
-MY_MGMT_IP=$(get_node_ip_in_network "controller" "mgmt")
+CONTROLLER_MGMT_IP=$(get_node_ip_in_network "controller" "mgmt")
 
-# Replace variables with constants and remove lines used to fill in variables
+# Replace variables with constants and keep only lines starting with "export"
 
-cat "$CONFIG_DIR/admin-openstackrc.sh" | sed -e "
-    /BASH_SOURCE/d
-    /^source/d
+cat "$CONFIG_DIR/admin-openstackrc.sh" | sed -ne "
     s/\$ADMIN_PROJECT_NAME/$ADMIN_PROJECT_NAME/
     s/\$ADMIN_USER_NAME/$ADMIN_USER_NAME/
     s/\$ADMIN_PASS/$ADMIN_PASS/
-    s/controller/$MY_MGMT_IP/
+    s/controller/$CONTROLLER_MGMT_IP/
+    /^export/p
     " > "$HOME/admin-openrc.sh"
 
-cat "$CONFIG_DIR/demo-openstackrc.sh" | sed -e "
-    /BASH_SOURCE/d
-    /^source/d
+cat "$CONFIG_DIR/demo-openstackrc.sh" | sed -ne "
     s/\$DEMO_PROJECT_NAME/$DEMO_PROJECT_NAME/
     s/\$DEMO_USER_NAME/$DEMO_USER_NAME/
     s/\$DEMO_PASS/$DEMO_PASS/
-    s/controller/$MY_MGMT_IP/
+    s/controller/$CONTROLLER_MGMT_IP/
+    /^export/p
     " > "$HOME/demo-openrc.sh"
