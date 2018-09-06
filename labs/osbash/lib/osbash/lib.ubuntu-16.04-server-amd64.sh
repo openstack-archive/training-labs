18f0419bc418c9a0f3e4b870f4820fe1ad2a9e87
Roger Luethi <rl@patchworkscience.org>
2018-09-06 08:47:07 +0200

    Update Ubuntu LTS ISO to 16.04.5
    backport: queens pike
    
    Change-Id: Ia70e64c4506b71e97d5c4e485e014d5b7c6cc4a0

--- labs/osbash/lib/osbash/lib.ubuntu-16.04-server-amd64.sh
+++ labs/osbash/lib/osbash/lib.ubuntu-16.04-server-amd64.sh
@@ -9,9 +9,9 @@
 
 readonly ISO_URL_BASE=http://releases.ubuntu.com/16.04
 
-ISO_URL=$ISO_URL_BASE/ubuntu-16.04.4-server-amd64.iso
+ISO_URL=$ISO_URL_BASE/ubuntu-16.04.5-server-amd64.iso
 #ISO_MD5=23e97cd5d4145d4105fbf29878534049
-ISO_MD5=6a7f31eb125a0b2908cf2333d7777c82
+ISO_MD5=24636fd103a2a43c95659f1c3c63718e
 
 readonly _PS_ssh=http://git.openstack.org/cgit/openstack/training-labs/plain/labs/osbash/lib/osbash/netboot/preseed-ssh-v6.cfg
 readonly _PS_vbadd=http://git.openstack.org/cgit/openstack/training-labs/plain/labs/osbash/lib/osbash/netboot/preseed-vbadd-v6.cfg
