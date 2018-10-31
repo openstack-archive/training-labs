a9886a7ded56bfd2514fa85a1633afc9b48e986d
Slawek Kaplonski <skaplons@redhat.com>
2018-10-31 13:06:32 +0100

    remove external_network_bridge option
    The external_network_bridge option is deprecated/legacy and being
    removed from neutron [1].
    This patch removes the external_network_bridge option iniset from
    training-labs repo.
    
    [1] https://review.openstack.org/#/c/567369/
    
    Change-Id: Iad270d755185ac8caa2566ee0cc4dfe4df317907

--- labs/osbash/scripts/ubuntu/setup_self-service_controller.sh
+++ labs/osbash/scripts/ubuntu/setup_self-service_controller.sh
@@ -145,12 +145,6 @@ echo "Configuring the layer-3 agent."
 conf=/etc/neutron/l3_agent.ini
 iniset_sudo $conf DEFAULT interface_driver linuxbridge
 
-# FIXME The following block (comment and change to external_network_bridge
-# disappeared in the docs transition to Pike. Intentionally?
-# The external_network_bridge option intentionally lacks a value to enable
-# multiple external networks on a single agent.
-iniset_sudo $conf DEFAULT external_network_bridge ""
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Configure the DHCP agent
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
